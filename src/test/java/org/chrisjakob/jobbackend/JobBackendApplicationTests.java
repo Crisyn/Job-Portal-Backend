3d76d4a67c709b3623e386c48b3d67cebb2c0aec
Crisyn <125381193+Crisyn@users.noreply.github.com>
2024-04-15 09:21:41 +0200

    first commit

---
+++ src/test/java/org/chrisjakob/jobbackend/JobBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.chrisjakob.jobbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
